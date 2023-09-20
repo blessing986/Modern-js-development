057e7bfb3e5cb360f84c5188f3a75967f9436cf8
Blessing U <blessing986@users.noreply.github.com>
2023-09-20 22:02:48 +0100

    chore: Importing and exporting ES6 modules.

---
+++ script.js
@@ -0,0 +1,22 @@
+// Importing module
+
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+console.log('Importing module');
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+
+////////////////////////////////////
+// Named export
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+// Default export
+import add from './shoppingCart.js';
+add('pizza', 2);
+
