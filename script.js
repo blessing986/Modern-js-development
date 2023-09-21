3d188dbaa0738c45d438997fcba73f0535613c54
Blessing U <blessing986@users.noreply.github.com>
2023-09-21 12:26:02 +0100

    chore: Top level await (ES2022)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+///////////////////////////////////////
+// Exporting and Importing in ES6 Modules
 // Importing module
 
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
@@ -10,13 +12,42 @@ console.log('Importing module');
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
-
 ////////////////////////////////////
 // Named export
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // console.log(price);
 
 // Default export
-import add from './shoppingCart.js';
+import add, { cart } from './shoppingCart.js';
 add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
+console.log(cart);
+
+///////////////////////////////////////
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// await keyword works outside an async function in module
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+// Top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
